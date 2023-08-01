b0c115a6bb9693bc34b744fb192df467c0ffbc8e
Zeinab-Saboori <140276954+Zeinab-Saboori@users.noreply.github.com>
2023-08-01 06:58:30 -0700

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,42 +1,89 @@
 import math
-num1= float(input("Enter your first number:"))
-num2= float(input("Enter your second number:"))
 
-print("Choose an operation:")
-print("1.Addition")
-print("2.Subtraction")
-print("3.Multiplication")
-print("4.Division")
-print("5.Radical")
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def square_root(x):
+    return math.sqrt(x)
+
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x-1)
+
+def sin(x):
+    return math.sin(x)
+
+def cos(x):
+    return math.cos(x)
+
+def tan(x):
+    return math.tan(x)
+
+def cot(x):
+    return 1 / math.tan(x)
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Square root")
 print("6.Factorial")
-print("7.Sine")
-print("8.Cosine")
-print("9.Tangent")
-print("10.cotangent")
+print("7.Sin")
+print("8.Cos")
+print("9.Tan")
+print("10.Cot")
 
-choice=int(input("Enter choice:"))
+while True:
+    choice = input("Enter choice(1/2/3/4/5/6/7/8/9/10): ")
 
-if choice==1:
-    result=num1+num2
-elif choice==2:
-    result=num1-num2
-elif choice==3:
-    result=num1*num2
-elif choice==4:
-    result=num1/num2
-elif choice==5:
-   result=math.sqrt(num1)+math.sqrt(num2)
-elif choice==6:
-    result=math.factorial(num1)+math.factorial(num2)
-elif choice==7:
-    result=math.sin(num1)+math.sin(num2)
-elif choice==8:
-    result=math.cos(num1)+math.cos(num2)
-elif choice==9:
-    result=math.tan(num1)+math.tan(num2)
-elif choice==10:
-    result=1/math.tan(num1)+1/math.tan(num2)
-else:
-    print("Invalid input")
-print("The result is:", result)
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+    elif choice in ('5', '6', '7', '8', '9', '10'):
+        num = float(input("Enter a number: "))
+
+        if choice == '5':
+            print("Square root of", num, "=", square_root(num))
+
+        elif choice == '6':
+            print("Factorial of", num, "=", factorial(num))
+
+        elif choice == '7':
+            print("Sin(", num, ") =", sin(num))
+
+        elif choice == '8':
+            print("Cos(", num, ") =", cos(num))
+
+        elif choice == '9':
+            print("Tan(", num, ") =", tan(num))
+
+        elif choice == '10':
+            print("Cot(", num, ") =", cot(num))
+    else:
+        print("Invalid Input")
 
